26ca9985f7570428b5ce46e1b1dc58815460dcf7
shenjiaqing <js4198@rit.edu>
2020-10-24 02:52:02 +0800

    update fault

---
+++ src/lc735.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+
+/**
+ * @author Jiaqing Shen
+ * @description
+ * @date Created in 2020/10/21
+ */
+public class lc735 {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < asteroids.length; ++i) {
+            while (!stack.isEmpty() && asteroids[i] < 0 && stack.peek() > 0 &&
+                    stack.peek() < -asteroids[i]) {
+                stack.pop();
+            }
+
+
+            if (stack.isEmpty() || stack.peek() < 0 || asteroids[i] > 0) stack.add(asteroids[i]);
+            else if (stack.peek() == -asteroids[i]) stack.pop();
+        }
+        if (stack.size() == 0) return new int[]{};
+        int[] res = new int[stack.size()];
+        for (int i = res.length - 1; i >= 0; --i) {
+            res[i] = stack.pop();
+        }
+        return res;
+    }
+}
