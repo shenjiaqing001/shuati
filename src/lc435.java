5aba34aacc4588c9570cdc2f6820aedec891653f
shenjiaqing <js4198@rit.edu>
2020-11-16 03:53:16 +0800

    435 贪心。617 合并二叉树。每日一题。

--- src/lc435.java
+++ src/lc435.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * @author Jiaqing Shen
@@ -6,5 +8,16 @@ import java.util.Arrays;
  * @date Created in 8/16/20
  */
 public class lc435 {
-
+    public int eraseOverlapIntervals(int[][] intervals) {
+        if (intervals.length == 0) return 0;
+        Arrays.sort(intervals, Comparator.comparingInt(o -> o[1]));
+        int res = 1;
+        int last = intervals[0][1];
+        for (int i = 1; i < intervals.length; ++i) {
+            if (intervals[i][0] < last) continue;
+            res++;
+            last = intervals[i][1];
+        }
+        return intervals.length - res;
+    }
 }
