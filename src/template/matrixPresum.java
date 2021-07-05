48599dd49468bc5b70feaf18198d1af8082b992e
shenjiaqing <js4198@rit.edu>
2021-07-05 02:05:42 -0400

    pow.java 快速幂模版 matrixPresum matrix的模版

---
+++ src/template/matrixPresum.java
@@ -0,0 +1,35 @@
+package template;
+
+/**
+ * @author Jiaqing Shen
+ * @description
+ * @date Created in 2021/7/5
+ */
+public class matrixPresum {
+
+    public void presum(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                dp[i + 1][j + 1] = dp[i + 1][j] + matrix[i][j];
+            }
+        }
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = m - 1; j >= 0; --j) {
+                dp[i + 1][j + 1] += dp[i][j + 1];
+            }
+        }
+//            for (int i = 0; i <= n; ++i) {
+//                for (int j = 0; j <= m; ++j) {
+//                    System.out.print(dp[i][j]);
+//                }
+//                System.out.println();
+//            }
+
+    }
+
+}
---
+++ src/template/pow.java
@@ -0,0 +1,21 @@
+package template;
+
+/**
+ * @author Jiaqing Shen
+ * @description
+ * @date Created in 2021/7/4
+ */
+public class pow {
+    private static int pow(int n, int m) {
+        int res = 1;
+        int base = n;
+        while (m != 0) {
+            if ((m & 1) == 1) {
+                res = (res * base) % 1_000_000_007;
+            }
+            base = (base * base) % 1_000_000_007;
+            m = m >> 1;
+        }
+        return res;
+    }
+}
