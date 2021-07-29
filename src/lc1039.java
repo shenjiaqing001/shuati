acdd1100bfd668ef409ac4344a37e9bc5db54f9b
shenjiaqing <js4198@rit.edu>
2021-07-29 00:25:07 -0400

    1039 dp 区间dp

---
+++ src/lc1039.java
@@ -0,0 +1,20 @@
+/**
+ * @author Jiaqing Shen
+ * @description
+ * @date Created in 2021/7/25
+ */
+public class lc1039 {
+    public int minScoreTriangulation(int[] A) {
+        int n = A.length;
+        int[][] dp = new int[n][n];
+        for (int d = 2; d < n; ++d) {
+            for (int i = 0; i + d < n; ++i) {
+                int j = i + d;
+                dp[i][j] = Integer.MAX_VALUE;
+                for (int k = i + 1; k < j; ++k)
+                    dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k][j] + A[i] * A[j] * A[k]);
+            }
+        }
+        return dp[0][n - 1];
+    }
+}
