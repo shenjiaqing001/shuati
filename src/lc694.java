f21f7a1d9b14f867b14a56ff6428347712bf846b
shenjiaqing <js4198@rit.edu>
2021-08-05 23:53:17 -0400

    lc694 bfs hash

--- src/lc694.java
+++ src/lc694.java
@@ -1,7 +1,4 @@
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author Jiaqing Shen
@@ -13,7 +10,6 @@ public class lc694 {
         if (grid.length == 0 || grid[0].length == 0) return 0;
         int x = grid.length;
         int y = grid[0].length;
-        int res = 0;
         Set<String> set = new HashSet<>();
 
         for (int i = 0; i < x; ++i) {
@@ -28,9 +24,6 @@ public class lc694 {
         return set.size();
     }
 
-
-    int[][] dir = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-
     public void dfs(int[][] grid, int x, int y, StringBuffer sb, String dir) {
         if (x < 0 || y < 0 || x >= grid.length || y >= grid[0].length)
             return;
@@ -43,5 +36,54 @@ public class lc694 {
         dfs(grid, x, y + 1, sb, "r");
         dfs(grid, x, y - 1, sb, "l");
         sb.append("b");
+    }
+
+    public int numDistinctIslands2(int[][] grid) {
+        if (grid.length == 0 || grid[0].length == 0) return 0;
+        int x = grid.length;
+        int y = grid[0].length;
+        Set<String> set = new HashSet<>();
+
+        for (int i = 0; i < x; ++i) {
+            for (int j = 0; j < y; ++j) {
+                if (grid[i][j] == 1) {
+                    set.add(bfs(grid, i, j));
+                }
+            }
+        }
+        return set.size();
+    }
+
+    int[][] dir = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+    public String bfs(int[][] grid, int x, int y) {
+        List<int[]> list = new ArrayList<>();
+        int m = grid.length;
+        int n = grid[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{x, y});
+        grid[x][y] = 0;
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+            list.add(new int[]{now[0] - x, now[1] - y});
+            for (int i = 0; i < 4; ++i) {
+                int nx = now[0] + dir[i][0];
+                int ny = now[1] + dir[i][1];
+                if (nx < 0 || ny < 0 || nx >= grid.length || ny >= grid[0].length)
+                    continue;
+                if (grid[nx][ny] == 0)
+                    continue;
+                grid[nx][ny] = 0;
+                queue.add(new int[]{nx, ny});
+            }
+        }
+        list.sort((o1, o2) -> (o1[0] == o2[0] ? o1[1] - o2[1] : o1[0] - o2[0]));
+
+        StringBuilder sb = new StringBuilder();
+        for (int[] xy : list) {
+            sb.append(xy[0] + "," + xy[1] + ";");
+        }
+        System.out.println(sb.toString());
+        return sb.toString();
     }
 }
