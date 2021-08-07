b2dd4255719c170a12e2f95424c62e5b3e108d10
shenjiaqing <js4198@rit.edu>
2021-08-07 18:32:36 -0400

    319 math

---
+++ src/lc319.java
@@ -0,0 +1,10 @@
+/**
+ * @author Jiaqing Shen
+ * @description
+ * @date Created in 2021/8/1
+ */
+public class lc319 {
+    public int bulbSwitch(int n) {
+        return (int) Math.sqrt(n);
+    }
+}
