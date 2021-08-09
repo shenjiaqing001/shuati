c5dd19ed8127b7592fd2b4b4cf8f2e6fcd4eff38
shenjiaqing <js4198@rit.edu>
2021-08-09 10:23:17 -0400

    1 hashmap

--- src/lc1.java
+++ src/lc1.java
@@ -1,7 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Jiaqing Shen
  * @description
  * @date Created in 2020/9/30
  */
 public class lc1 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (map.containsKey(target - nums[i])) return new int[]{i, map.get(target - nums[i])};
+            map.put(nums[i], i);
+        }
+        return null;
+    }
 }
