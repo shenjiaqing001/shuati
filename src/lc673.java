79103a8b9a9310a34e236d50b23fc8956f026697
shenjiaqing <js4198@rit.edu>
2021-08-10 21:50:08 -0400

    673 dp

--- src/lc673.java
+++ src/lc673.java
@@ -27,4 +27,43 @@ public class lc673 {
         }
         return res;
     }
+
+    public int findNumberOfLIS2(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n][2];
+
+        dp[0][0] = 1;
+        dp[0][1] = 1;
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[j] < nums[i]) {
+                    if (dp[j][0] >= dp[i][0]) {
+                        dp[i][0] = dp[j][0] + 1;
+                        dp[i][1] = dp[j][1];
+                    } else if (dp[j][0] + 1 == dp[i][0]) {
+                        dp[i][1] += dp[j][1];
+                    }
+                }
+            }
+            if (dp[i][0] == 0) {
+                dp[i][0] = 1;
+                dp[i][1] = 1;
+            }
+        }
+
+        int max = 0;
+        int count = 0;
+
+        for (int i = 0; i < n; ++i) {
+            if (dp[i][0] > max) {
+                max = dp[i][0];
+                count = dp[i][1];
+            } else if (dp[i][0] == max) {
+                count += dp[i][1];
+            }
+        }
+
+        return count;
+    }
 }
