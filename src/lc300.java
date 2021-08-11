f74c87f897ed4b55ccf308a549709f774eff87f7
shenjiaqing <js4198@rit.edu>
2021-08-10 21:49:19 -0400

    300 dp

--- src/lc300.java
+++ src/lc300.java
@@ -4,7 +4,6 @@
  * @date Created in 2020/9/18
  */
 public class lc300 {
-
     public int lengthOfLIS(int[] nums) {
         int[] lis = new int[nums.length];
         int size = 0;
@@ -23,8 +22,23 @@ public class lc300 {
             if (lo == size) size++;
 
         }
-
         return size;
+    }
+
+    public int lengthOfLIS2(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        int res = 0;
+
+        for (int i = 0; i < n; ++i) {
+            int max = 0;
+            for (int j = 0; j < i; ++j) {
+                if (nums[j] < nums[i]) max = Math.max(max, dp[j]);
+            }
+            dp[i] = max + 1;
+            res = Math.max(res, dp[i]);
+        }
+        return res;
     }
 
 }
