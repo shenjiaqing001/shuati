b9b5a28e75e42a6a8b48c6c2fe83f46b64373da7
shenjiaqing <js4198@rit.edu>
2021-08-25 01:01:58 -0400

    329 dfs + memo

--- src/lc329.java
+++ src/lc329.java
@@ -6,13 +6,11 @@
 public class lc329 {
     public int longestIncreasingPath(int[][] matrix) {
         if (matrix.length == 0) return 0;
-        int x = matrix.length;
-        int y = matrix[0].length;
-        int[][] dp = new int[x][y];
+        int[][] dp = new int[matrix.length][matrix[0].length];
         int max = 1;
-        for (int i = 0; i < x; ++i) {
-            for (int j = 0; j < y; j++) {
-                int length = dfs(matrix, dp, x, y, i, j);
+        for (int i = 0; i < matrix.length; ++i) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                int length = dfs(matrix, dp, i, j);
                 max = Math.max(max, length);
             }
         }
@@ -21,20 +19,20 @@ public class lc329 {
 
     public int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
-    public int dfs(int[][] matrix, int[][] dp, int x, int y, int i, int j) {
-        if (dp[i][j] != 0) return dp[i][j];
+    public int dfs(int[][] matrix, int[][] dp, int x, int y) {
+        if (dp[x][y] != 0) return dp[x][y];
         int max = 1;
-        for (int k = 0; k < 4; ++k) {
-            int nextx = i + dirs[k][0];
-            int nexty = j + dirs[k][1];
-            if (nextx < 0 || nexty < 0 || nextx >= x || nexty >= y ||
-                    matrix[nextx][nexty] <= matrix[i][j]) {
+        for (int i = 0; i < 4; ++i) {
+            int nextx = x + dirs[i][0];
+            int nexty = y + dirs[i][1];
+            if (nextx < 0 || nexty < 0 || nextx >= matrix.length || nexty >= matrix[0].length ||
+                    matrix[nextx][nexty] <= matrix[x][y]) {
                 continue;
             }
-            int length = 1 + dfs(matrix, dp, x, y, nextx, nexty);
+            int length = 1 + dfs(matrix, dp, nextx, nexty);
             max = Math.max(length, max);
         }
-        dp[i][j] = max;
+        dp[x][y] = max;
         return max;
     }
 }
