fd18c4390ac616f2b59212df699821b791e750d2
shenjiaqing <js4198@rit.edu>
2021-08-25 08:38:11 -0400

    276 dp

--- src/lc276.java
+++ src/lc276.java
@@ -20,4 +20,15 @@ public class lc276 {
         }
         return lastSum;
     }
+
+    public int numWays2(int n, int k) {
+        int[][] dp = new int[n][2];
+        dp[0][0] = k;
+        dp[0][1] = 0;
+        for (int i = 1; i < n; ++i) {
+            dp[i][0] = (dp[i - 1][0] + dp[i - 1][1]) * (k - 1);
+            dp[i][1] = dp[i - 1][0];
+        }
+        return dp[n - 1][0] + dp[n - 1][1];
+    }
 }
