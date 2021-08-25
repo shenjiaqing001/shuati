17659bbc4ce36cb5b5f5b950ee7deea983f3734e
shenjiaqing <js4198@rit.edu>
2021-08-25 18:04:35 -0400

    1556 string

--- src/BiweeklyContest33/lc1556.java
+++ src/BiweeklyContest33/lc1556.java
@@ -5,7 +5,7 @@ package BiweeklyContest33;
  * @description
  * @date Created in 8/22/20
  */
-public class lc1 {
+public class lc1556 {
     public String thousandSeparator(int n) {
         if (n < 1000) return String.valueOf(n);
         String res = "";
@@ -13,7 +13,7 @@ public class lc1 {
             int now = n % 1000;
             n /= 1000;
             res = String.valueOf(now) + "." + res;
-            if(n > 0){
+            if (n > 0) {
                 if (now < 10) res = "00" + res;
                 else if (now < 100) res = "0" + res;
             }
