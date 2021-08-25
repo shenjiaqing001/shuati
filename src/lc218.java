2c6596c265888599cc242a59e88b94eaa85d591e
shenjiaqing <js4198@rit.edu>
2021-08-24 22:08:22 -0400

    218 pq treemap sort

--- src/lc218.java
+++ src/lc218.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 /**
  * @author Jiaqing Shen
@@ -38,4 +35,39 @@ public class lc218 {
         }
         return res;
     }
+
+    public List<List<Integer>> getSkyline2(int[][] buildings) {
+        List<List<Integer>> res = new ArrayList<>();
+        int n = buildings.length;
+        int[][] heights = new int[n * 2][];
+        for (int i = 0; i < n; ++i) {
+            heights[2 * i] = new int[]{buildings[i][0], buildings[i][2]};
+            heights[2 * i + 1] = new int[]{buildings[i][1], -buildings[i][2]};
+        }
+        Arrays.sort(heights, (o1, o2) -> (
+                o1[0] == o2[0] ? Integer.compare(o2[1], o1[1]) : Integer.compare(o1[0], o2[0])));
+
+        TreeMap<Integer, Integer> map = new TreeMap<>(Collections.reverseOrder());
+        map.put(0, 0);
+        int last = 0;
+        for (int[] height : heights) {
+            if (height[1] > 0) {
+                map.put(height[1], map.getOrDefault(height[1], 0) + 1);
+            } else {
+                map.put(-height[1], map.get(-height[1]) - 1);
+                if (map.get(-height[1]) == 0) map.remove(-height[1]);
+            }
+            int now = map.firstKey();
+            if (last != now) {
+                List<Integer> tmp = new ArrayList<>();
+                tmp.add(height[0]);
+                tmp.add(now);
+                res.add(tmp);
+                last = now;
+            }
+        }
+        return res;
+    }
+
+
 }
