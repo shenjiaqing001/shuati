91ad28f51e5818fb3e8745eb32d6b841e378f40c
shenjiaqing <js4198@rit.edu>
2021-08-30 09:34:09 -0400

    647 manacher palindrome

--- src/lc647.java
+++ src/lc647.java
@@ -23,4 +23,39 @@ public class lc647 {
             right++;
         }
     }
+
+    public int countSubstrings2(String s) {
+        if (s == null || s.equals("")) return 0;
+        char[] str = s.toCharArray();
+        char[] c = new char[s.length() * 2 + 1];
+
+        for (int i = 0; i < c.length; ++i) {
+            c[i] = i % 2 == 0 ? '#' : str[i / 2];
+        }
+
+        int[] radius = new int[c.length];
+        int center = -1;
+        int maxRight = -1;
+        int res = 0;
+        for (int i = 0; i < c.length; ++i) {
+            int r = 0;
+            if (i <= maxRight) {
+                r = Math.min(radius[center * 2 - i], maxRight - i);
+            }
+
+            while (i + r < c.length && i - r >= 0 &&
+                    c[i - r] == c[i + r]) {
+                r++;
+            }
+
+            radius[i] = r - 1;
+            if (i + radius[i] > maxRight) {
+                maxRight = i + radius[i];
+                center = i;
+            }
+            res += r / 2;
+        }
+        return res;
+    }
+
 }
--- src/z/template/manacher.java
+++ src/z/template/manacher.java
@@ -8,40 +8,36 @@ package z.template;
 public class manacher {
 
 
-    public static void main(String[] args) {
-        manacher test = new manacher();
-        System.out.println(test.maxPalindrome("aab"));
-    }
+    public int manacher(String s) {
+        if (s == null || s.equals("")) return 0;
+        char[] str = s.toCharArray();
+        char[] c = new char[s.length() * 2 + 1];
 
-    public int maxPalindrome(String string) {
-        if (string == null || string == "") return 0;
-        if (string.length() == 1) return 1;
-
-        StringBuilder sb = new StringBuilder();
-        sb.append("#");
-        for (char c : string.toCharArray()) {
-            sb.append(c);
-            sb.append("#");
+        for (int i = 0; i < c.length; ++i) {
+            c[i] = i % 2 == 0 ? '#' : str[i / 2];
         }
 
-        char[] c = sb.toString().toCharArray();
         int[] radius = new int[c.length];
         int center = -1;
-        int r = -1;
-        int max = 0;
+        int maxRight = -1;
         for (int i = 0; i < c.length; ++i) {
-            radius[i] = r > i ? Math.min(radius[2 * center - r], r - i + 1) : 1;
-            while (i + radius[i] < c.length && i - radius[i] > -1) {
-                if (c[i - radius[i]] == c[i + radius[i]]) {
-                    radius[i]++;
-                } else break;
+            int r = 0;
+            if (i <= maxRight) {
+                r = Math.min(radius[center * 2 - i], maxRight - i);
             }
-            if (i + radius[i] > r) {
-                r = i + radius[i] - 1;
+
+            while (i + r < c.length && i - r >= 0 &&
+                    c[i - r] == c[i + r]) {
+                r++;
+            }
+
+            radius[i] = r - 1;
+            if (i + radius[i] > maxRight) {
+                maxRight = i + radius[i];
                 center = i;
             }
-            max = Math.max(max, radius[i]);
         }
-        return max - 1;
+        return 0;
     }
+
 }
