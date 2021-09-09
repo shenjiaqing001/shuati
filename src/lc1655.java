94413487ff96bdde554ef3f5c75b9aa5abef9ac2
shenjiaqing <js4198@rit.edu>
2021-09-09 01:21:35 -0400

    1655 dfs+memo/dp+bitmask

--- src/lc1655.java
+++ src/lc1655.java
@@ -1,4 +1,6 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Jiaqing Shen
@@ -33,5 +35,48 @@ public class lc1655 {
             }
         }
         return false;
+    }
+
+
+    public static void main(String[] args) {
+        lc1655 test = new lc1655();
+        System.out.println(test.canDistribute2(new int[]{1, 1, 2, 3}, new int[]{2, 2}));
+    }
+
+    public boolean canDistribute2(int[] nums, int[] quantity) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        int m = map.size();
+        int n = quantity.length;
+        int index = 0;
+        int[] count = new int[m];
+        for (Integer value : map.values()) count[index++] = value;
+
+        boolean[][] dp = new boolean[m][1 << n];
+        for (int i = 0; i < m; ++i) dp[i][0] = true;
+        for (int i = 0; i < m; ++i) {
+            for (int state = 1; state < (1 << n); ++state) {
+                if (i > 0 && dp[i - 1][state]) {
+                    dp[i][state] = true;
+                    continue;
+                }
+                for (int subset = state; subset > 0; subset = (subset - 1) & state) {
+                    if (i > 0 && !dp[i - 1][state - subset]) continue;
+                    if (i == 0 && state != subset) continue;
+
+                    int sum = 0;
+                    for (int k = 0; k < n; ++k) {
+                        if (((subset >> k) & 1) > 0) sum += quantity[k];
+                    }
+                    if (sum <= count[i]) {
+                        dp[i][state] = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return dp[m - 1][(1 << n) - 1];
     }
 }
