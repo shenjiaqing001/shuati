06bd300e515c5c428997f6a0c3e3b5e94b017dbd
shenjiaqing <js4198@rit.edu>
2021-09-14 02:28:57 -0400

    1627 union find

--- src/WeeklyContest211/lc1627.java
+++ src/WeeklyContest211/lc1627.java
@@ -9,34 +9,41 @@ import java.util.List;
  * @date Created in 2020/10/18
  */
 public class lc1627 {
-    int[] p;
-
-    int get(int x) {
-        return p[x] == x ? x : (p[x] = get(p[x]));
-    }
-
-    void union(int x, int y) {
-        p[get(x)] = p[get(y)];
-    }
-
     public List<Boolean> areConnected(int n, int threshold, int[][] queries) {
-        p = new int[n + 1];
-        for (int i = 1; i <= n; ++i) p[i] = i;
-
+        UnionFind uf = new UnionFind(n);
         for (int i = threshold + 1; i <= n; ++i) {
-            for (int j = 2; ; ++j) {
-                if (i * j > n) break;
-                union(i, i * j);
+            for (int j = 2; i * j <= n; ++j) {
+                uf.union(i, i * j);
             }
         }
-        List<Boolean> res = new ArrayList<>();
 
+        List<Boolean> res = new ArrayList<>();
         for (int i = 0; i < queries.length; ++i) {
-            if (get(queries[i][0]) == get(queries[i][1]))
+            if (uf.get(queries[i][0]) == uf.get(queries[i][1]))
                 res.add(true);
             else
                 res.add(false);
         }
         return res;
+    }
+
+    public class UnionFind {
+        int[] p;
+
+        public UnionFind(int n) {
+            p = new int[n + 1];
+            for (int i = 0; i <= n; ++i) p[i] = i;
+        }
+
+        public int get(int x) {
+            return p[x] == x ? x : (p[x] = get(p[x]));
+        }
+
+        public void union(int x, int y) {
+            int a = get(x);
+            int b = get(y);
+            if (p[a] < p[b]) p[b] = p[a];
+            else p[a] = p[b];
+        }
     }
 }
