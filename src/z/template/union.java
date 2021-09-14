d882045c765b690da64bdc81df204c52bee8d64d
shenjiaqing <js4198@rit.edu>
2021-09-14 02:19:35 -0400

    update union find template

--- src/z/template/union.java
+++ src/z/template/union.java
@@ -6,17 +6,25 @@ package z.template;
  * @date Created in 8/31/20
  */
 public class union {
-    int[] p;
 
-    int get(int x) {
-        return p[x] == x ? x : (p[x] = get(p[x]));
-    }
+    public class UnionFind {
+        int[] p;
 
-    void union(int x, int y) {
-        int a = get(x);
-        int b = get(y);
-        if (p[a] < p[b]) p[b] = p[a];
-        else p[a] = p[b];
+        public UnionFind(int n) {
+            p = new int[n];
+            for (int i = 0; i < n; ++i) p[i] = i;
+        }
+
+        public int get(int x) {
+            return p[x] == x ? x : (p[x] = get(p[x]));
+        }
+
+        public void union(int x, int y) {
+            int a = get(x);
+            int b = get(y);
+            if (p[a] < p[b]) p[b] = p[a];
+            else p[a] = p[b];
+        }
     }
 
 }
