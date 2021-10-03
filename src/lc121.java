f2932c65d5a696aaf42a1f413d9878cbec6c524a
shenjiaqing <js4198@rit.edu>
2021-10-03 15:26:05 -0400

    121 array

--- src/lc121.java
+++ src/lc121.java
@@ -18,4 +18,24 @@ public class lc121 {
         }
         return res;
     }
+
+    public int maxProfit2(int[] prices) {
+        int res = 0;
+        int max = 0;
+        int min = 0;
+
+
+        for (int i = 0; i < prices.length; ++i) {
+            if (prices[i] < max) {
+                res += Math.max(0, max - min);
+                min = prices[i];
+                max = min;
+            } else {
+                max = prices[i];
+            }
+        }
+        res += max - min;
+
+        return res;
+    }
 }
