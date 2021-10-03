ba03dc8b40624376294eb1bcbe3d30da6141af5d
shenjiaqing <js4198@rit.edu>
2021-10-03 15:19:24 -0400

    update pair problem

--- src/lc636.java
+++ src/lc636.java
@@ -1,37 +1,38 @@
-//import javafx.util.Pair;
-//
-//import java.util.List;
-//import java.util.Stack;
-//
-///**
-// * @author Jiaqing Shen
-// * @description
-// * @date Created in 2020/10/7
-// */
-//public class lc636 {
-//    public int[] exclusiveTime(int n, List<String> logs) {
-//        Stack<Pair<Integer, Integer>> stack = new Stack<>();
-//        int[] res = new int[n];
-//        int time = 0;
-//
-//        for (String str : logs) {
-//            String[] log = str.split(":");
-//            if (log[1].equals("end")) {
-//                Pair<Integer, Integer> pop = stack.pop();
-//                res[pop.getKey()] += Integer.valueOf(log[2]) - pop.getValue() + 1;
-//                if (!stack.isEmpty()) {
-//                    stack.add(new Pair<>(stack.pop().getKey(), Integer.valueOf(log[2]) + 1));
-//                }
-//            } else {
-//                if (!stack.isEmpty()) {
-//                    Pair<Integer, Integer> pop = stack.peek();
-//                    res[pop.getKey()] += Integer.valueOf(log[2]) - pop.getValue();
-//                }
-//                stack.add(new Pair<>(Integer.valueOf(log[0]), Integer.valueOf(log[2])));
-//            }
-//        }
-//        return res;
-//    }
-//
-//
-//}
+import java.util.AbstractMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * @author Jiaqing Shen
+ * @description
+ * @date Created in 2020/10/7
+ */
+public class lc636 {
+    public int[] exclusiveTime(int n, List<String> logs) {
+        Stack<Map.Entry<Integer, Integer>> stack = new Stack<>();
+        int[] res = new int[n];
+
+        for (String str : logs) {
+            String[] log = str.split(":");
+            if (log[1].equals("end")) {
+                Map.Entry<Integer, Integer> pop = stack.pop();
+                res[pop.getKey()] += Integer.parseInt(log[2]) - pop.getValue() + 1;
+                if (!stack.isEmpty()) {
+                    stack.add(new AbstractMap.SimpleEntry<>(stack.pop().getKey(),
+                            Integer.parseInt(log[2]) + 1));
+                }
+            } else {
+                if (!stack.isEmpty()) {
+                    Map.Entry<Integer, Integer> pop = stack.peek();
+                    res[pop.getKey()] += Integer.parseInt(log[2]) - pop.getValue();
+                }
+                stack.add(new AbstractMap.SimpleEntry<>(Integer.valueOf(log[0]),
+                        Integer.valueOf(log[2])));
+            }
+        }
+        return res;
+    }
+
+
+}
