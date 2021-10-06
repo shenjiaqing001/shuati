d946bb86be28a758cc2d819e63e03841dddf4f7e
shenjiaqing <js4198@rit.edu>
2021-10-06 00:47:53 -0400

    1277 dp

--- src/lc1277.java
+++ src/lc1277.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 /**
  * @author Jiaqing Shen
  * @description
@@ -30,6 +28,23 @@ public class lc1277 {
                         dp[i][j] = 1;
                         res += 1;
                     }
+                }
+            }
+        }
+        return res;
+    }
+
+
+    public int countSquares2(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[][] dp = new int[n + 1][m + 1];
+        int res = 0;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                if (matrix[i - 1][j - 1] == 1) {
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    res += dp[i][j];
                 }
             }
         }
