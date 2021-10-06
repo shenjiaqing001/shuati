c9bcc1999a3b333309d84e98b3870f3c2c0f73af
shenjiaqing <js4198@rit.edu>
2021-10-06 17:38:58 -0400

    496 stack

---
+++ src/lc496.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * @author Jiaqing Shen
+ * @description
+ * @date Created in 2021/10/5
+ */
+public class lc496 {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> stack = new Stack<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                map.put(stack.pop(), num);
+            }
+            stack.add(num);
+        }
+        int[] res = new int[nums1.length];
+        for (int i = 0; i < nums1.length; ++i) {
+            res[i] = map.getOrDefault(nums1[i], -1);
+        }
+        return res;
+    }
+}
