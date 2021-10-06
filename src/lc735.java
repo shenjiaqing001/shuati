a5b46479b0e333e995fe5c2fa45db809610b30e2
shenjiaqing <js4198@rit.edu>
2021-10-06 00:37:22 -0400

    739 stack

--- src/lc735.java
+++ src/lc735.java
@@ -25,4 +25,27 @@ public class lc735 {
         }
         return res;
     }
+
+    public int[] asteroidCollision2(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+        for (int asteroid : asteroids) {
+            if (asteroid < 0) {
+                while (!stack.isEmpty() && stack.peek() > 0 && stack.peek() < -asteroid) {
+                    stack.pop();
+                }
+                if (!stack.isEmpty() && stack.peek() == -asteroid) stack.pop();
+                else {
+                    if (stack.isEmpty() || stack.peek() < 0) stack.add(asteroid);
+                }
+            } else {
+                stack.add(asteroid);
+            }
+        }
+        int[] res = new int[stack.size()];
+        for (int i = res.length - 1; i >= 0; --i) {
+            res[i] = stack.pop();
+        }
+        return res;
+    }
+
 }
