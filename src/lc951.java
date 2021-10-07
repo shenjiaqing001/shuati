c023f8027517dd96fe6141bf57711862a49bd499
shenjiaqing <js4198@rit.edu>
2021-10-07 02:26:31 -0400

    951 dfs

--- src/lc951.java
+++ src/lc951.java
@@ -8,8 +8,18 @@ public class lc951 {
         if (root1 == null && root2 == null) return true;
         if (root1 == null || root2 == null) return root1 == root2;
 
-        return root1.val == root2.val &&  flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left) ||
+        return root1.val == root2.val && flipEquiv(root1.left, root2.right) &&
+                flipEquiv(root1.right, root2.left) ||
                 flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right);
     }
 
+
+    public boolean flipEquiv2(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) return true;
+        if (root1 == null || root2 == null) return root1 == root2;
+
+        return root1.val == root2.val &&
+                ((flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right)) ||
+                        (flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left)));
+    }
 }
