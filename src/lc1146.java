3be01401757eee61323e21f9a7fde0a94f032a9c
shenjiaqing <js4198@rit.edu>
2021-10-09 21:07:29 -0400

    1146 treemap

--- src/lc1146.java
+++ src/lc1146.java
@@ -1,4 +1,6 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
 
 /**
  * @author Jiaqing Shen
@@ -32,6 +34,32 @@ public class lc1146 {
 
         public int get(int index, int snap_id) {
             return list.get(index).floorEntry(snap_id).getValue();
+        }
+    }
+
+    class SnapshotArray2 {
+
+        TreeMap<Integer, Integer>[] list;
+        int time = 0;
+
+        public SnapshotArray2(int length) {
+            list = new TreeMap[length];
+            for (int i = 0; i < length; ++i) {
+                list[i] = new TreeMap<>();
+                list[i].put(0, 0);
+            }
+        }
+
+        public void set(int index, int val) {
+            list[index].put(time, val);
+        }
+
+        public int snap() {
+            return time++;
+        }
+
+        public int get(int index, int snap_id) {
+            return list[index].floorEntry(snap_id).getValue();
         }
     }
 }
