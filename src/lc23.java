fe686c49e2fc61e3a525f9b5f15f2adff25169e0
shenjiaqing <js4198@rit.edu>
2021-10-15 00:51:04 -0400

    23 pq

--- src/lc23.java
+++ src/lc23.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.PriorityQueue;
 
 /**
@@ -8,12 +10,7 @@ import java.util.PriorityQueue;
  */
 public class lc23 {
     public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<ListNode> pq = new PriorityQueue<>(new Comparator<ListNode>() {
-            @Override
-            public int compare(ListNode o1, ListNode o2) {
-                return o1.val - o2.val;
-            }
-        });
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o.val));
         ListNode head = new ListNode();
         ListNode now = head;
 
@@ -32,5 +29,32 @@ public class lc23 {
         }
 
         return head.next;
+    }
+
+    public List<Integer> mergeKLists2(ListNode[] lists, int k) {
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o.val));
+        List<Integer> res = new ArrayList<>();
+
+        for (ListNode listNode : lists) {
+            if (listNode != null)
+                pq.add(listNode);
+        }
+
+        while (!pq.isEmpty()) {
+            int num = pq.peek().val;
+            int count = 0;
+            while (!pq.isEmpty() && pq.peek().val == num) {
+                ListNode tmp = pq.poll();
+                count++;
+                while (tmp != null && tmp.val == num) {
+                    tmp = tmp.next;
+                }
+                if (tmp != null) {
+                    pq.add(tmp);
+                }
+            }
+            if (count >= k) res.add(num);
+        }
+        return res;
     }
 }
