29da0f38fcd954b57db7e6c7f3e5d2ae6eda45c8
shenjiaqing <js4198@rit.edu>
2021-10-17 17:59:33 -0400

    837 dp

--- src/lc837.java
+++ src/lc837.java
@@ -5,21 +5,20 @@
  */
 public class lc837 {
     public static void main(String[] args) {
-        System.out.println(1.0 / 55);
-        int maxPts = 10;
-        double prob = (1.0 / ((maxPts) * (maxPts + 1) / 2));
-        System.out.println(prob);
-
+        lc837 test = new lc837();
+        test.new21Game(21, 17, 10);
     }
 
     public double new21Game(int n, int k, int maxPts) {
+        if (k == 0) return 1;
         double remain = 1;
         double res = 0;
-        double[] dp = new double[n + 1];
-        for (int i = 1; i <= n; ++i) {
+        double[] dp = new double[k + maxPts];
+        dp[0] = 1;
+        for (int i = 1; i <= k + maxPts - 1; ++i) {
             dp[i] = remain / maxPts;
             if (i < k) remain += dp[i];
-            else res += dp[i];
+            if (i >= k && i <= n) res += dp[i];
             if (i >= maxPts) {
                 remain -= dp[i - maxPts];
             }
