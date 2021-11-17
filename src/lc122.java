6e732738762649e21e56f00b4b9820bf878b2bdc
shenjiaqing001 <664657291@qq.com>
2021-11-16 23:27:24 -0500

    122 easy

--- src/lc122.java
+++ src/lc122.java
@@ -5,8 +5,20 @@
  */
 public class lc122 {
     public int maxProfit(int[] prices) {
+        if (prices.length <= 1) return 0;
+        int res = 0;
+        int low = prices[0];
+        int high = prices[0];
 
-
-        return 0;
+        for (int i = 1; i < prices.length; ++i) {
+            if (prices[i] < high) {
+                res += (high - low);
+                low = high = prices[i];
+            } else {
+                high = prices[i];
+            }
+        }
+        res += (high - low);
+        return res;
     }
 }
