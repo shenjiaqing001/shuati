d9cc82f6b666304513af2b7a771f5079ed426c54
shenjiaqing001 <664657291@qq.com>
2021-12-01 22:27:50 -0500

    708 coding too many edge cases

--- src/lc708.java
+++ src/lc708.java
@@ -10,37 +10,39 @@ public class lc708 {
             head.next = head;
             return head;
         }
-        if (head.next == head) {
-            head.next = new Node(insertVal);
-            head.next.next = head;
-            return head;
-        }
 
         Node now = head;
         while (true) {
             Node next = now.next;
-            if (now.val > next.val) {
-                if (insertVal >= now.val || insertVal <= next.val) {
-                    Node tmp = new Node(insertVal);
-                    tmp.next = next;
-                    now.next = tmp;
-                    return head;
-                }
-            } else {
-                if (insertVal >= now.val && insertVal <= next.val) {
-                    Node tmp = new Node(insertVal);
-                    tmp.next = next;
-                    now.next = tmp;
-                    return head;
-                }
-            }
-            now = now.next;
-            if (now == head) {
+            if (next == head) {
                 Node tmp = new Node(insertVal);
-                tmp.next = next;
                 now.next = tmp;
+                tmp.next = next;
                 return head;
             }
+
+            if (insertVal >= now.val && insertVal <= next.val) {
+                Node tmp = new Node(insertVal);
+                now.next = tmp;
+                tmp.next = next;
+                return head;
+            }
+            if (next.val < now.val) {
+                if (insertVal >= now.val) {
+                    Node tmp = new Node(insertVal);
+                    now.next = tmp;
+                    tmp.next = next;
+                    return head;
+                }
+                if (insertVal <= now.val && insertVal <= next.val) {
+                    Node tmp = new Node(insertVal);
+                    now.next = tmp;
+                    tmp.next = next;
+                    return head;
+                }
+
+            }
+            now = now.next;
         }
     }
 }
